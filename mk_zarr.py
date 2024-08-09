aa86ba173674795f7f52874a2d74af5a0fe71c40
misumi-cr <misumi@criepi.denken.or.jp>
2024-08-09 17:29:20 +0900

    modified

--- mk_zarr.py
+++ mk_zarr.py
@@ -30,9 +30,12 @@ def rename_dims(ds):
                     })
     return ds
 
-def process_file(file, variables_to_merge):
+#def process_file(file, variables_to_merge):
+#    ds = xr.open_dataset(file, chunks={'ocean_time': -1})
+#    return ds[variables_to_merge]
+def process_file(file):
     ds = xr.open_dataset(file, chunks={'ocean_time': -1})
-    return ds[variables_to_merge]
+    return ds
 
 def process_grid(file):
     ds = xr.open_dataset(file)
@@ -115,42 +118,44 @@ def set_time(ds):
     ds=ds.set_coords(["fig_time"])
     return ds
 
-grid_name='/data44/misumi/obtn_zarr/obtn_mount_adcp-z5_grd-17cm_nearest_rx10.nc'
-case_name='obtn_h040_s05.135'
-variables_to_merge = ['temp', 'salt']
-src_dir=f'/data44/misumi/roms_out/{case_name}/out'
-dst_dir=f'/data44/misumi/roms_zarr_test'
 
+if __name__ == "__main__":
 
-# グリッドファイル取得と処理
-ds_grid=xr.open_dataset(grid_name)
-ds_grid=ds_grid.drop_vars(['hraw','lon_vert','lat_vert','x_vert','y_vert','spherical'])
-ds_grid=select_interior(ds_grid)
-
-# ファイルリストを取得
-files=sorted(glob.glob(f'{src_dir}/{case_name}.a.00[1-5].nc'))
-
-ds0=[process_file(f, variables_to_merge) for f in files]
-
-# xarray.concatを使用してデータセットを結合
-ds0_concat=xr.concat(ds0, dim='ocean_time')
-
-# 重複する時間を削除（必要な場合）
-unique_times = ~pd.Index(ds0_concat.ocean_time.values).duplicated(keep='first')
-ds0_concat = ds0_concat.isel(ocean_time=unique_times)
-
-ds0_concat=select_interior(ds0_concat)
-ds0_concat=xr.merge([ds0_concat,ds_grid])
-ds0_concat=rename_dims(ds0_concat)
-
-ds0_concat=add_coords(ds0_concat)
-ds0_concat=set_time(ds0_concat)
-ds0_concat=ds0_concat.chunk({'time': 1})
-grid=Grid(ds0_concat, coords={'X': {'center': 'xh', 'outer': 'xq'},
-                              'Y': {'center': 'yh', 'outer': 'yq'},
-                              'Z': {'center': 's_rho', 'outer': 's_w'}},
-          periodic=False)
-ds0_concat=compute_depth_layers(ds0_concat,ds_grid)
-#
-## 結果をZarr形式で保存
-#ds0_concat.chunk({'ocean_time': 1}).to_zarr(f'{dst_dir}/{case_name}')
+    grid_name='/data44/misumi/obtn_zarr/obtn_mount_adcp-z5_grd-17cm_nearest_rx10.nc'
+    case_name='obtn_h040_s05.135'
+#    variables_to_merge = ['temp', 'salt']
+    src_dir=f'/data44/misumi/roms_out/{case_name}/out'
+    dst_dir=f'/data44/misumi/roms_zarr_test'
+    
+    # グリッドファイル取得と処理
+    ds_grid=xr.open_dataset(grid_name)
+    ds_grid=ds_grid.drop_vars(['hraw','lon_vert','lat_vert','x_vert','y_vert','spherical'])
+    ds_grid=select_interior(ds_grid)
+    
+    # ファイルリストを取得
+    files=sorted(glob.glob(f'{src_dir}/{case_name}.a.00[1-5].nc'))
+    
+    ds0=[process_file(f) for f in files]
+    
+    # xarray.concatを使用してデータセットを結合
+    ds0_concat=xr.concat(ds0, dim='ocean_time')
+    
+    # 重複する時間を削除（必要な場合）
+    unique_times = ~pd.Index(ds0_concat.ocean_time.values).duplicated(keep='first')
+    ds0_concat = ds0_concat.isel(ocean_time=unique_times)
+    
+    ds0_concat=select_interior(ds0_concat)
+    ds0_concat=xr.merge([ds0_concat,ds_grid])
+    ds0_concat=rename_dims(ds0_concat)
+    
+    ds0_concat=add_coords(ds0_concat)
+    ds0_concat=set_time(ds0_concat)
+    ds0_concat=ds0_concat.chunk({'time': 1})
+    grid=Grid(ds0_concat, coords={'X': {'center': 'xh', 'outer': 'xq'},
+                                  'Y': {'center': 'yh', 'outer': 'yq'},
+                                  'Z': {'center': 's_rho', 'outer': 's_w'}},
+              periodic=False)
+    ds0_concat=compute_depth_layers(ds0_concat,ds_grid)
+    #
+    ## 結果をZarr形式で保存
+    #ds0_concat.chunk({'ocean_time': 1}).to_zarr(f'{dst_dir}/{case_name}')
