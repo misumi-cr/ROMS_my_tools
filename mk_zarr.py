0b33e7af8ef8cc4710498b78344d124035eb53b1
misumi-cr <misumi@criepi.denken.or.jp>
2024-08-09 16:30:51 +0900

    Modified

--- mk_zarr.py
+++ mk_zarr.py
@@ -38,6 +38,83 @@ def process_grid(file):
     ds = xr.open_dataset(file)
     return ds
 
+def add_coords(ds):
+    """ set coordinate variables as xarray coordinates
+        Parameters:
+            ds (xarray.Dataset): ROMS dataset
+    """
+    ds = ds.set_coords(['Cs_r', 'Cs_w', 'hc', 'h', 'Vtransform', 'time',
+                        'lon_rho', 'lon_v', 'lon_u', 'lon_psi',
+                        'lat_rho', 'lat_v', 'lat_u', 'lat_psi'])
+    return ds
+
+def flist_cut(flist):
+    newlist=[]
+    for f in flist:
+        wrk0=f.split("/")[-1]
+        wrk1=int(wrk0.split(".")[-2])
+        if wrk1>=99 and wrk1<=100:
+            newlist.append(f)
+    return newlist
+
+def append_time(store_name):
+    ds0=xr.open_zarr(store_name)
+    ds1=xr.Dataset()
+    mdl_time=[]
+    fig_time=[]
+    for tm in ds0["time"].data:
+        mdl_time.append(dt.datetime(2000+tm.year,tm.month,tm.day,tm.hour,0,0))
+        fig_time.append(dt.datetime(2004,tm.month,tm.day,tm.hour,0,0))
+    ds1["mdl_time"]=xr.DataArray(np.array(mdl_time),dims=["time"])
+    ds1["fig_time"]=xr.DataArray(np.array(fig_time),dims=["time"])
+    ds1=ds1.set_coords(["mdl_time","fig_time"])
+    ds1.to_zarr(store_name,mode="a")
+
+def compute_depth_layers(ds, grid, hmin=-0.1):
+    """ compute depths of ROMS vertical levels (Vtransform = 2) """
+    
+    # compute vertical transformation functional
+    S_rho = (ds.hc * ds.s_rho + ds.Cs_r * ds.h) / (ds.hc + ds.h)
+    S_w = (ds.hc * ds.s_w + ds.Cs_w * ds.h) / (ds.hc + ds.h)
+    
+    # compute depth of rho (layers) and w (interfaces) points
+    z_rho = ds.zeta + (ds.zeta + ds.h) * S_rho
+    z_w = ds.zeta + (ds.zeta + ds.h) * S_w
+    
+    z_rho.data=ma.masked_outside(z_rho.data,-1.e4,1.e4)
+    z_w.data=ma.masked_outside(z_w.data,-1.e4,1.e4)
+    
+    # transpose arrays and fill NaNs with a minimal depth
+    ds['z_rho'] = z_rho.transpose(*('time', 's_rho','yh','xh'),
+                                  transpose_coords=False).fillna(hmin)
+    
+    ds['z_w'] = z_w.transpose(*('time', 's_w','yh','xh'),
+                                  transpose_coords=False).fillna(hmin)
+    
+    # interpolate depth of levels at U and V points
+    ds['z_u'] = grid.interp(ds['z_rho'], 'X', boundary='fill')
+    ds['z_v'] = grid.interp(ds['z_rho'], 'Y', boundary='fill')
+    
+    # compute layer thickness as difference between interfaces
+    ds['dz'] = grid.diff(ds['z_w'], 'Z')
+    
+    # add z_rho and z_w to xarray coordinates
+    ds = ds.set_coords(['z_rho', 'z_w', 'z_v', 'z_u'])
+    
+    return ds
+
+def set_time(ds):
+    mdl_time=[]
+    fig_time=[]
+    for tm in ds["time"].data:
+        mdl_time.append(dt.datetime(2000+tm.year,tm.month,tm.day,tm.hour,0,0))
+        fig_time.append(dt.datetime(2000,tm.month,tm.day,tm.hour,0,0))
+    ds["mdl_time"]=xr.DataArray(np.array(mdl_time),dims=["time"])
+    ds["fig_time"]=xr.DataArray(np.array(fig_time),dims=["time"])
+    ds=ds.set_index(time="mdl_time")
+    ds=ds.set_coords(["fig_time"])
+    return ds
+
 grid_name='/data44/misumi/obtn_zarr/obtn_mount_adcp-z5_grd-17cm_nearest_rx10.nc'
 case_name='obtn_h040_s05.135'
 variables_to_merge = ['temp', 'salt']
@@ -65,6 +142,15 @@ ds0_concat = ds0_concat.isel(ocean_time=unique_times)
 ds0_concat=select_interior(ds0_concat)
 ds0_concat=xr.merge([ds0_concat,ds_grid])
 ds0_concat=rename_dims(ds0_concat)
+
+ds0_concat=add_coords(ds0_concat)
+ds0_concat=set_time(ds0_concat)
+ds0_concat=ds0_concat.chunk({'time': 1})
+grid=Grid(ds0_concat, coords={'X': {'center': 'xh', 'outer': 'xq'},
+                              'Y': {'center': 'yh', 'outer': 'yq'},
+                              'Z': {'center': 's_rho', 'outer': 's_w'}},
+          periodic=False)
+ds0_concat=compute_depth_layers(ds0_concat,ds_grid)
 #
 ## 結果をZarr形式で保存
 #ds0_concat.chunk({'ocean_time': 1}).to_zarr(f'{dst_dir}/{case_name}')
