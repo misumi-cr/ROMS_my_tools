f4e235d43200f3cf8e696ffbd6709ecd90685d33
misumi-cr <misumi@criepi.denken.or.jp>
2024-08-09 10:59:56 +0900

    bug fix

--- mk_zarr.py
+++ mk_zarr.py
@@ -6,7 +6,6 @@ import glob
 
 def process_file(file, variables_to_merge):
     ds = xr.open_dataset(file, chunks={'ocean_time': -1})
-    # 指定された変数のみを選択
     return ds[variables_to_merge]
 
 case_name='obtn_h040_s05.135'
@@ -23,11 +22,11 @@ lazy_datasets = [dask.delayed(process_file)(f, variables_to_merge) for f in file
 # 遅延オブジェクトを計算し、データセットのリストを取得
 datasets = dask.compute(*lazy_datasets)
 
-# xarray.mergeを使用してデータセットを結合
-merged_ds = xr.merge(datasets, compat='equals', join='outer')
+# xarray.concatを使用してデータセットを結合
+concat_ds = xr.concat(datasets, dim='ocean_time')
 
 # 重複する時間を削除（必要な場合）
-merged_ds = merged_ds.sel(time=~merged_ds.indexes['ocean_time'].duplicated(keep='first'))
+concat_ds = concat_ds.sel(ocean_time=~concat_ds.ocean_time.duplicated(keep='first'))
 
 # 結果をZarr形式で保存
-merged_ds.chunk({'ocean_time': 1}).to_zarr(f'{dst_dir}/{case_name}')
+concat_ds.chunk({'ocean_time': 1}).to_zarr(f'{dst_dir}/{case_name}')
